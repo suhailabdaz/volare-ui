0bbfbe01330dbcb0771b8f0a5a8be60b3dda023e
suhailabdaz <suhail.abdaz@gmail.com>
2024-07-01 12:25:23 +0530

    structure_V_0.0.1

--- src/pages/user/Home/HomePage.tsx
+++ src/pages/user/Home/HomePage.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Navbar from '../../../components/user/Home/Homepage/Navbar'
+import HeroSection from '../../../components/user/Home/Homepage/HeroSection'
+import Banner from '../../../components/user/Home/Homepage/Banner'
+import Offers from '../../../components/user/Home/Homepage/Offers'
+import About from '../../../components/user/Home/Homepage/About'
+import Popular from '../../../components/user/Home/Homepage/Popular'
+import Links from '../../../components/user/Home/Homepage/Links'
+import Footer from '../../../components/user/Home/Homepage/Footer'
 
 function HomePage() {
   return (
-    <div><h1> Home</h1></div>
+    <>
+    <Navbar/>
+    <HeroSection/>
+    <Banner/>
+    <Offers/>
+    <About/>
+    <Popular/>
+    <Links/>
+    <Footer/>
+    </>
   )
 }
 
