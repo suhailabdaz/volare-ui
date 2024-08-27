4a5ac925ecfa1a0bf93204fc02be70ea3011aad6
suhailabdaz <suhail.abdaz@gmail.com>
2024-08-27 16:35:24 +0530

    pre-searching

--- src/utils/socket/socket.ts
+++ src/utils/socket/socket.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000', {
+const socket = io(import.meta.env.VITE_API_GATEWAY_BASE_URL, {
     transports: ['websocket'],
     withCredentials: true
 });
